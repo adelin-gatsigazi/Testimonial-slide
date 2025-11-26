aaf785662855f2d59e2d4480680f17fd2009d53b
gatsigazi-adelin <gatsigazi.adelin@gmail.com>
2025-11-26 04:06:42 +0200

    fixed the comment

--- script.js
+++ script.js
@@ -1,42 +1,45 @@
 const testimonials = [
     {
-    photoUrl: 
-        "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=880&q=80",
-        text: "This is simply unbelievable! I would be lost without Apple. The very best. Not able to tell you how happy I am with Apple.",
-        name: "Cherise G",
+    photoUrl:"Images/Cherise G.jpg",
+    text: "This is simply unbelievable! I would be lost without Apple. The very best. Not able to tell you how happy I am with Apple.",
+    name: "Cherise G",
     },
     {
-    photoUrl:
-      "https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=880&q=80",
+    photoUrl:"Images/Rosetta Q.jpg",
     text: "I would also like to say thank you to all your staff. Wow what great service, I love it! Apple impressed me on multiple levels.",
     name: "Rosetta Q",
   },
   {
-    photoUrl:
-      "https://images.unsplash.com/photo-1628157588553-5eeea00af15c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=880&q=80",
+    photoUrl:"Images/Constantine V.jpg",
     text: "Thank you for making it painless, pleasant and most of all hassle free! I wish I would have thought of it first. The very best.",
     name: "Constantine V",
-  }, 
+  },
 ];
 
 const imgEl = document.querySelector("img");
 const textEl = document.querySelector(".text");
 const nameEl = document.querySelector(".name");
 
-let idx = 0;
+if (imgEl && textEl && nameEl) {
+
+let currentIndex = 0;
 
 updateTestimonial();
 
-function updateTestimonial () {
-    const {name, photoUrl, text} = testimonials[idx];
+function updateTestimonial() {
+    const {name, photoUrl, text} = testimonials[currentIndex];
     imgEl.src = photoUrl;
     textEl.innerHTML = text;
     nameEl.innerText = name;
-    idx++;
-    if(idx === testimonials.length) {
-        idx = 0;
+    
+    currentIndex++;
+    
+    if(currentIndex === testimonials.length) {
+        currentIndex = 0;
     }
+    
     setTimeout(() => {
         updateTestimonial();
     }, 1000);
+}
 }
